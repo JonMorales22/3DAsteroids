03bb2999def3405f225ae176cab8ca97a5129305
Jonathan Morales <jonmorales22@gmail.com>
2016-11-28 14:29:16 -0500

    Got the movement functinality and the player can fire projectiles.

--- Assets/_scripts/CamerController.cs
+++ Assets/_scripts/CamerController.cs
@@ -4,76 +4,61 @@ using System.Collections;
 public class CamerController : MonoBehaviour {
 	public float speedH = 2.0f;
 	public float speedV = 2.0f;
+	public float speedR = 2.0f;
+
+	public float thrust = 100.0f;
+	public float missleForce = 500.0f;
 
 	public float min;
 	public float max;
 
+	public GameObject missle;
+	public GameObject spawn;
+
 	private float yaw = 0.0f;
 	private float pitch = 0.0f;
-
+	private float roll = 0.0f;
+	private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-
+		rb = GetComponent<Rigidbody> ();
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		yaw += speedH * Input.GetAxis("Mouse X");
-		//yaw=0;
 		pitch -= speedV * Input.GetAxis("Mouse Y");
-		//pitch=0;
-		Vector3 deltaVec = new Vector3 (pitch, yaw, 0.0f);
+
+		if (Input.GetKey (KeyCode.LeftArrow))
+			roll += speedR;
+		else if (Input.GetKey (KeyCode.RightArrow))
+			roll -= speedR;
+		
+		Vector3 deltaVec = new Vector3 (pitch, yaw, roll);
 		deltaVec.x = Mathf.Clamp (deltaVec.x, min, max);
+
 		transform.eulerAngles = deltaVec;
+		if (Input.GetMouseButtonDown (0) || Input.GetKeyDown (KeyCode.Space))
+			Fire ();
 
-		//Debug.Log (pitch);
-		/*
-		Vector2 pos = Camera.main.ScreenToViewportPoint (Input.mousePosition);
-		Vector2 deltaVec = (center - pos)*-1;
-
-		float delta = ((pos.x - center.x) * (pos.x - center.x))+((pos.y - center.y) * (pos.y - center.y));
-		float distance = Mathf.Sqrt (delta);
-
-		if (distance < .01)
-			distance = 0;
-
-		float x = Input.GetAxis ("Mouse X");
-		float y = Input.GetAxis ("Mouse Y");
-
-		//Debug.Log ("x: " + x );
-		//Debug.Log ("y: " + y );
-		Debug.Log ("Distance: " + distance);
-		Debug.Log("Pos: "+pos);
-		Debug.Log("DeltaVec: "+deltaVec);
-		if(Mathf.Abs(deltaVec.x)>0||Mathf.Abs(deltaVec.y)>0)
-		{
-			//if (distance < 1.0)
-			transform.rotation = Quaternion.Euler (transform.rotation.Euler, 0, 0);
-			//else if (distance >= 1.0)
-				//transform.rotation = Quaternion.Euler (2, 2, 0);
-		}
-		//float rotationX = Input.GetAxis("Mouse X") * xSpeed;
-		//transform.Rotate(0, rotationX, 0);
-		//float rotationY = Input.GetAxis("Mouse Y") * ySpeed;
-
-		//transform.Rotate(rotationY, 0, 0);
-		//transform.LookAt(center);
-		//Debug.Log(distance);
-		//transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(pos.y*ySpeed,pos.x*xSpeed,0), 5.0f);
-		//if(Input.GetKeyDown(KeyCode.LeftArrow))
-		//	transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 5);
-
-	*/
+		if (Input.GetKey (KeyCode.UpArrow))
+			rb.AddForce (transform.forward * thrust);
+		else if (Input.GetKey (KeyCode.DownArrow))
+			rb.AddForce (transform.forward * thrust*-1);
 	}
 
-	public float getYaw()
+	public void Fire()
 	{
-		return yaw;
+		GameObject foo = (GameObject)Instantiate (missle, transform.forward+transform.position, Quaternion.identity);
+		Rigidbody rb = foo.GetComponent<Rigidbody> ();
+		rb.AddForce (transform.forward * missleForce);
 	}
 
-	public float getPitch()
+	public void Thrust()
 	{
-		return pitch;
+
+
 	}
 }
---
+++ Assets/_scripts/MissleController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissleController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, 5.0f);
+	}
+	
+	void OnCollisionEnter(Collision c)
+	{
+		if (c.gameObject.CompareTag ("Meteor")) {
+			Destroy (c.gameObject);
+			Destroy(gameObject);
+		}
+	}
+		
+}
